1a4e182a1d561b22b214ff1a42fe524e625e990e
JaviMx <javimxmine@hotmail.com>
2022-04-22 14:52:18 -0500

    Creacion de server

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//Usando objeto express
+const express = require('express')
+
+//app de express
+const app = express()
+
+//Puerto en que vamos a ver nuestra app: localhost:9000
+const port = 9000
+
+//Path inicial, responderá a la url localhost:9000
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
+
+//Con esto inicializamos esta app
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`)
+})
